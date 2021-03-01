956e350af64c4a9e5b376f57f2da816cebf81575
soosungp33 <soosungp33@gmail.com>
2021-03-01 17:16:44 +0900

    주석 설명

--- chat/main.go
+++ chat/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // 경로에 요청이 오는지 수신 대기(요청이 오면 HTML 보내기)
 		w.Write([]byte(`
 		<html>
 		  <head>
@@ -20,7 +20,7 @@ func main() {
 	})
 
 	// 	웹 서버 시작
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil) // 8080 포트에서 웹 서버 시작
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}

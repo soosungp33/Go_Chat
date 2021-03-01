e1f12e9866850613d3236f2b35791450513d1e19
soosungp33 <soosungp33@gmail.com>
2021-03-01 17:15:49 +0900

    간단한 http 작동

--- chat/main.go
+++ chat/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+		<html>
+		  <head>
+		    <title>Chat</title>
+		  </head>
+		  <body>
+		    Let's chat!
+		  </body>
+		</html>
+		`))
+	})
+
+	// 	웹 서버 시작
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }

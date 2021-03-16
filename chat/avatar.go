449702eb59a90511670a5e8416fb1281397103f2
soosungp33 <soosungp33@gmail.com>
2021-03-16 14:00:57 +0900

    TDD방식으로 인터페이스 변경, 기존 구현 수정(아직 빌드 오류남)

--- chat/avatar.go
+++ chat/avatar.go
@@ -14,7 +14,7 @@ var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL.")
 type Avatar interface {
 	// GetAvatarURL은 지정된 클라이언트에 대한 아바타 URL을 가져오고, 문제가 발생하면 에러를 리턴한다.
 	// 객체가 지정된 클라이언트의 URL을 가져올 수 없는 경우 ErrNoAvatarURL이 리턴된다.
-	GetAvatarURL(c *client) (string, error) // URL을 리턴할 사용자를 알 수 있도록 클라이언트를 인수로 사용
+	GetAvatarURL(ChatUser) (string, error) // URL을 리턴할 사용자를 알 수 있도록 클라이언트를 인수로 사용
 }
 
 type AuthAvatar struct{}
@@ -22,13 +22,20 @@ type AuthAvatar struct{}
 var UseAuthAvatar AuthAvatar
 
 // 객체가 nil값을 가질 수 있으므로 리시버의 변수명을 생략해 Go에 참조를 버리라고 전달
-func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
-	if url, ok := c.userData["avatar_url"]; ok {
-		if urlStr, ok := url.(string); ok { // 사용자 데이터가 있으면
-			return urlStr, nil
-		}
+func (AuthAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	url := u.AvatarURL()
+	if len(url) == 0 {
+		return "", ErrNoAvatarURL
 	}
-	return "", ErrNoAvatarURL
+	return url, nil
+	/*
+		if url, ok := c.userData["avatar_url"]; ok {
+			if urlStr, ok := url.(string); ok { // 사용자 데이터가 있으면
+				return urlStr, nil
+			}
+		}
+		return "", ErrNoAvatarURL
+	*/
 }
 
 // AuthAvatar와 같은 패턴
@@ -37,37 +44,50 @@ type GravatarAvatar struct{}
 var UseGravatar GravatarAvatar
 
 // 객체가 nil값을 가질 수 있으므로 리시버의 변수명을 생략해 Go에 참조를 버리라고 전달
-func (GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	if userid, ok := c.userData["userid"]; ok {
-		if useridStr, ok := userid.(string); ok { // 사용자 데이터가 있으면
-			return "//www.gravatar.com/avatar/" + useridStr, nil // 기본 URL에 추가
+func (GravatarAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	return "//www.gravatar.com/avatar/" + u.UniqueID(), nil
+	/*
+		if userid, ok := c.userData["userid"]; ok {
+			if useridStr, ok := userid.(string); ok { // 사용자 데이터가 있으면
+				return "//www.gravatar.com/avatar/" + useridStr, nil // 기본 URL에 추가
+			}
 		}
-	}
-	return "", ErrNoAvatarURL
+		return "", ErrNoAvatarURL
+	*/
 }
 
 type FileSystemAvatar struct{}
 
 var UseFileSystemAvatar FileSystemAvatar
 
-func (FileSystemAvatar) GetAvatarURL(c *client) (string, error) {
-	if userid, ok := c.userData["userid"]; ok {
-		if useridStr, ok := userid.(string); ok {
-			files, err := ioutil.ReadDir("avatars") // avatars 폴데 있는 파일 목록을 전부 가져온다.(avatars 디렉터리도 포함됨) -> 뒤에 확장자까지 다 저장됨
-			if err != nil {
-				return "", ErrNoAvatarURL
+func (FileSystemAvatar) GetAvatarURL(u ChatUser) (string, error) {
+	if files, err := ioutil.ReadDir("avatars"); err == nil {
+		for _, file := range files {
+			if file.IsDir() {
+				continue
 			}
-			for _, file := range files {
-				if file.IsDir() { // 목록에는 디렉터리도 포함되므로 디렉터리면 건너뛴다.
-					continue
-				}
-				if match, _ := path.Match(useridStr+"*", file.Name()); match { // 각 파일이 userid와 일치하는지 확인
-					return "/avatars/" + file.Name(), nil // 일치하면 해당 파일을 찾은 것이므로 경로를 리턴
-				}
+			if match, _ := path.Match(u.UniqueID()+"*", file.Name()); match {
+				return "/avatars/" + file.Name(), nil
 			}
-			//return "/avatars/" + useridStr + ".jpg", nil // 하드코딩 -> jpg만 지원
 		}
 	}
+	/*
+		if userid, ok := c.userData["userid"]; ok {
+			if useridStr, ok := userid.(string); ok {
+				files, err := ioutil.ReadDir("avatars") // avatars 폴더에 있는 파일 목록을 전부 가져온다.(avatars 디렉터리도 포함됨) -> 뒤에 확장자까지 다 저장됨
+				if err != nil {
+					return "", ErrNoAvatarURL
+				}
+				for _, file := range files {
+					if file.IsDir() { // 목록에는 디렉터리도 포함되므로 디렉터리면 건너뛴다.
+						continue
+					}
+					if match, _ := path.Match(useridStr+"*", file.Name()); match { // 각 파일이 userid와 일치하는지 확인
+						return "/avatars/" + file.Name(), nil // 일치하면 해당 파일을 찾은 것이므로 경로를 리턴
+					}
+				}
+			}
+		}*/
 
 	return "", ErrNoAvatarURL
 }
--- chat/avatar_test.go
+++ chat/avatar_test.go
@@ -5,26 +5,28 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+
+	gomniauthtest "github.com/stretchr/gomniauth/test"
 )
 
 func TestAuthAvatar(t *testing.T) {
 	var authAvatar AuthAvatar
 
-	client := new(client) // 사용자 데이터가 없는 클라이언트를 사용해 ErrorNoAvatarURL이 리턴되는지 확인
-	url, err := authAvatar.GetAvatarURL(client)
+	testUser := &gomniauthtest.TestUser{}
+	testUser.On("AvatarURL").Return("", ErrNoAvatarURL) // 특정 메소드가 호출될 때 수행할 작업을 알림(여기서는 오류를 리턴)
+	testChatUser := &chatUser{User: testUser}
+	url, err := authAvatar.GetAvatarURL(testChatUser)
 	if err != ErrNoAvatarURL {
 		t.Error("AuthAvatar.GetAvatarURL should return ErrNoAvatarURL when no value present")
 	}
 
-	// 적절한 값을 설정해서
 	testUrl := "http://url-to-gravatar/"
-	client.userData = map[string]interface{}{
-		"avatar_url": testUrl,
-	}
-
-	url, err = authAvatar.GetAvatarURL(client) // 올바른 값을 리턴하는지 확인
+	testUser = &gomniauthtest.TestUser{}
+	testChatUser.User = testUser
+	testUser.On("AvatarURL").Return(testUrl, nil) // 여기서는 두 가지 결과를 시뮬레이션하는 testUrl 값을 리턴
+	url, err = authAvatar.GetAvatarURL(testChatUser)
 	if err != nil {
-		t.Error("AuthAvatar.GetAvatarURL should return no error when value present")
+		t.Error("AuthAvatar.GetAvatarURL should return ErrNoAvatarURL when no value present")
 	}
 	if url != testUrl {
 		t.Error("AuthAvatar.GetAvatarURL should return correct URL")
@@ -34,29 +36,25 @@ func TestAuthAvatar(t *testing.T) {
 func TestGravatarAvatar(t *testing.T) {
 	var gravatarAvatar GravatarAvatar
 
-	client := new(client)
-
-	client.userData = map[string]interface{}{ // gravatar는 이메일 주소의 해시를 사용해 각 프로필 이미지의 고유 ID를 생성
-		"uesrid": "a3b9630294061a6c02055a033f774f26",
-	}
-	url, err := gravatarAvatar.GetAvatarURL(client)
+	user := &chatUser{uniqueID: "abc"}
+	url, err := gravatarAvatar.GetAvatarURL(user)
 	if err != nil {
 		t.Error("GravatarAvatar.GetAvatarURL should not return an error")
 	}
-	if url != "//www.gravatar.com/avatar/a3b9630294061a6c02055a033f774f26" { // gravatar계정이 없으면 gravatar에 있는 임의의 이미지가 나온다.
+	if url != "//www.gravatar.com/avatar/abc" {
 		t.Errorf("GravatarAvatar.GetAvatarURL wrongly returned %s", url)
 	}
 }
 
 func TestFileSystemAvatar(t *testing.T) {
+	// 테스트 아바타 파일을 만듦
 	filename := filepath.Join("avatars", "abc.jpg")
 	ioutil.WriteFile(filename, []byte{}, 0777)
 	defer os.Remove(filename) // 테스트 코드를 자체적으로 삭제
 
 	var fileSystemAvatar FileSystemAvatar
-	client := new(client)
-	client.userData = map[string]interface{}{"userid": "abc"}
-	url, err := fileSystemAvatar.GetAvatarURL(client)
+	user := &chatUser{uniqueID: "abc"}
+	url, err := fileSystemAvatar.GetAvatarURL(user)
 	if err != nil {
 		t.Error("FileSystemAvatar.GetAvatarURL should not return an error")
 	}
